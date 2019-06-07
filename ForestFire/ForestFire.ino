52a6b67b9bc0c8a69baebd6352646c52e3c106f0
Jonathan Bobrow <jbobrow@gmail.com>
2019-06-07 01:05:30 -0400

    Update ForestFire.ino
    Sort of plotted individually, but still treating a tile as a the source of neighborhood

--- ForestFire/ForestFire.ino
+++ ForestFire/ForestFire.ino
@@ -1,16 +1,16 @@
 /*  Forest Fire
- *  by Jonathan Bobrow, Move38
- *  06/04/2019
- *  
- *  Press a Tree to control burn (no spreading)
- *  Long Press a Tree to strike lightning, it will ignite and then spread
- *  
- *  Plots of land grow trees (quicker around other Trees)... Each plot has
- *  up to 6 Trees
- *  The opportunity for a Tree to grow happens 3 times per second and fire
- *  spreads at this rate as well.
- *  The forest shares a heartbeat, so trees grow at the same time
- */
+    by Jonathan Bobrow, Move38
+    06/04/2019
+
+    Press a Tree to control burn (no spreading)
+    Long Press a Tree to strike lightning, it will ignite and then spread
+
+    Plots of land grow trees (quicker around other Trees)... Each plot has
+    up to 6 Trees
+    The opportunity for a Tree to grow happens 3 times per second and fire
+    spreads at this rate as well.
+    The forest shares a heartbeat, so trees grow at the same time
+*/
 
 // firefly style sync
 
@@ -21,7 +21,7 @@ enum LandType {
   WILD_FIRE
 };
 
-byte myLandType = SOIL;
+byte myLandType[6] = {SOIL, SOIL, SOIL, SOIL, SOIL, SOIL};
 
 #define LIGHT_DURATION 600
 Timer lighteningStrikeTimer;
@@ -39,63 +39,86 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(buttonSingleClicked()) {
-    myLandType = CTRL_FIRE;
+  if (buttonDoubleClicked()) {
+    FOREACH_FACE(f) {
+      setLandTypeOnFace(TREE, f);
+    }
   }
 
-  if(buttonLongPressed()) {
+  if (buttonSingleClicked()) {
+    FOREACH_FACE(f) {
+      setLandTypeOnFace(CTRL_FIRE, f);
+    }
+  }
+
+  if (buttonLongPressed()) {
     didLighteningStrike = true;
     lighteningStrikeTimer.set(LIGHT_DURATION);
   }
 
-  if(didLighteningStrike && lighteningStrikeTimer.isExpired()) {
-    myLandType = WILD_FIRE;
+  if (didLighteningStrike && lighteningStrikeTimer.isExpired()) {
+    FOREACH_FACE(f) {
+      setLandTypeOnFace(WILD_FIRE, f);
+    }
+
     didLighteningStrike = false;
   }
 
-  if(stepTimer.isExpired()) {
-  // ready to evaluate world
+  if (stepTimer.isExpired()) {
+    // ready to evaluate world
 
     // burn out after we have been fire
-    if(myLandType == WILD_FIRE || myLandType == CTRL_FIRE) {
-      myLandType = SOIL;  
-    }
-    
-    byte numTrees = 0;
-    
     FOREACH_FACE(f) {
-      if(!isValueReceivedOnFaceExpired(f)) {
+      if (myLandType[f] == WILD_FIRE || myLandType[f] == CTRL_FIRE) {
+        myLandType[f] = SOIL;
+      }
+    }
+
+    byte numTrees = 0;
+
+    FOREACH_FACE(f) {
+      if (!isValueReceivedOnFaceExpired(f)) {
         // we have a neighbor :)
         byte neighbor = getLastValueReceivedOnFace(f);
-  
-        if(neighbor == WILD_FIRE) {
+
+        if (neighbor == WILD_FIRE) {
           // then we catch fire
-          myLandType = WILD_FIRE;
+          myLandType[f] = WILD_FIRE;
         }
-        else if(neighbor == TREE) {
+        else if (neighbor == TREE) {
           numTrees++;
         }
       }
     }
 
     // turn soil into tree with a higher probability with more trees
-    if(random(10) < (numTrees + 4)) {
-      if(myLandType == SOIL) {
-        myLandType = TREE;
+    if (random(40) < (numTrees)) {
+      FOREACH_FACE(f) {
+        if (myLandType[f] == SOIL) {
+          setLandTypeOnFace(TREE, f);
+        }
       }
     }
-    
+
     stepTimer.set(STEP_DURATION);
   }
-  
+
   // DISPLAY STATES
-  switch(myLandType) {
-    case SOIL: setColor(BLUE);        break;
-    case TREE: setColor(GREEN);       break;
-    case CTRL_FIRE: setColor(ORANGE); break;
-    case WILD_FIRE: setColor(RED);    break;
+  FOREACH_FACE(f) {
+    switch (myLandType[f]) {
+      case SOIL:      setColorOnFace(BLUE, f);    break;
+      case TREE:      setColorOnFace(GREEN, f);   break;
+      case CTRL_FIRE: setColorOnFace(ORANGE, f);  break;
+      case WILD_FIRE: setColorOnFace(RED, f);     break;
+    }
   }
 
   // SHARE MY STATE
-  setValueSentOnAllFaces(myLandType);
+  FOREACH_FACE(f) {
+    setValueSentOnFace(myLandType[f], f);
+  }
 }
+
+void setLandTypeOnFace( byte type, byte face) {
+  myLandType[face] = type;
+};
